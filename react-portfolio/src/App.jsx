bf4cc3675afda8a7c1f9c285b96088f8511b8130
MuhamudhaAqsa <131351930+MuhamudhaAqsa@users.noreply.github.com>
2025-04-05 23:34:01 +0530

    Update App.jsx

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,28 +1,37 @@
-
+// Import CSS module styles
 import styles from "./App.module.css";
+
+// Import all the component sections of the website
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Experience } from "./components/Experience/Experience";
-
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar";
 import { Projects } from "./components/Projects/projects";
 
-
-
-
+// Root component of the application
 function App() {
-  
+  return (
+    <div className={styles.App}>
+      {/* Navigation bar at the top */}
+      <Navbar />
 
-  return <div className = {styles.App}>
-   <Navbar/>
-   <Hero/>
-   <About/>
-   <Experience/>
-   <Projects/>
-   <Contact/>
+      {/* Hero section - landing page intro */}
+      <Hero />
 
-  </div>;
+      {/* About section - personal background */}
+      <About />
+
+      {/* Experience section - work history or skills */}
+      <Experience />
+
+      {/* Projects section - portfolio work */}
+      <Projects />
+
+      {/* Contact section - get in touch form or details */}
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
