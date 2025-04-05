a142fcdb3df05871b5c2547784a6a16a40d84777
MuhamudhaAqsa <131351930+MuhamudhaAqsa@users.noreply.github.com>
2025-04-05 23:37:40 +0530

    Update utils.js

--- react-portfolio/src/utils.js
+++ react-portfolio/src/utils.js
@@ -1,3 +1,9 @@
+/**
+ * Generates a full URL for an image stored in the public assets folder.
+ * 
+ * @param {string} path - Relative path to the image inside the /assets directory.
+ * @returns {string} - Fully qualified URL that can be used in <img src=""> or CSS.
+ */
 export const getImageUrl = (path) => {
-    return new URL(`/assets/${path}`, import.meta.url).href;
-  };
+  return new URL(`/assets/${path}`, import.meta.url).href;
+};
