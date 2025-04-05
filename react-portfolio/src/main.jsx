d2b9c1a07e9746276087fb296362cbd1c2f787c4
MuhamudhaAqsa <131351930+MuhamudhaAqsa@users.noreply.github.com>
2025-04-05 23:34:46 +0530

    Update main.jsx

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -1,13 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// Import StrictMode to help highlight potential problems in an application
+import { StrictMode } from 'react';
 
+// Import createRoot to render the React app in the DOM
+import { createRoot } from 'react-dom/client';
+
+// Import global styles
+import './index.css';
+
+// Import the root App component
+import App from './App.jsx';
+
+// Import custom fonts
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 
+// Mount the React app to the root DOM node
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
